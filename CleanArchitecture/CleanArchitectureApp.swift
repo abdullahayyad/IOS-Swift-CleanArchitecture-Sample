498661bb90113208100a918e9433c3b6ba54a643
Abdullah Ayyad <ab.ayyad@gmail.com>
2022-04-18 20:50:28 +0300

    init project

--- CleanArchitecture/CleanArchitectureApp.swift
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct CleanArchitectureApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+            case .active:
+                print("scene is now active!")
+            case .inactive:
+                print("scene is now inactive!")
+            case .background:
+                print("scene is now in the background!")
+            @unknown default:
+                print("Apple must have added something new!")
+            }
         }
     }
 }
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CleanArchitecture' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'SwiftLint'
+
+  # Pods for CleanArchitecture
+
+  target 'CleanArchitectureTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'CleanArchitectureUITests' do
+    # Pods for testing
+  end
+
+end
