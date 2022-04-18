cd2644fe76271b3bef0671d774288e619933602d
Abdullah Ayyad <ab.ayyad@gmail.com>
2022-04-18 17:07:24 +0300

    Initial Commit

---
+++ CleanArchitecture/CleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureApp.swift
+//  CleanArchitecture
+//
+//  Created by Abdullah Ayyad on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
